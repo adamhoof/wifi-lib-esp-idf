1b582c1eea986cd0b673681eb56451215f96b1ec
adamhoof <adam.hofman1@gmail.com>
2024-06-02 19:11:58 +0200

    yeeeet

--- include/wifi-lib.h
+++ include/wifi-lib.h
@@ -1,5 +1,17 @@
 #pragma once
 
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
 
 /* The examples use WiFi configuration that you can set via project configuration menu
    If you'd rather not, just change the below entries to strings with
