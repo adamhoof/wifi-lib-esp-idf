2ea035c01016f046dc4094578e5d3f6030b47107
adamhoof <adam.hofman1@gmail.com>
2024-06-03 13:35:36 +0200

    move static definitions to .c file

--- include/wifi-lib.h
+++ include/wifi-lib.h
@@ -49,17 +49,10 @@
 #define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WAPI_PSK
 #endif
 
-/* FreeRTOS event group to signal when we are connected*/
-static EventGroupHandle_t s_wifi_event_group;
-
 /* The event group allows multiple bits for each event, but we only care about two events:
  * - we are connected to the AP with an IP
  * - we failed to connect after the maximum amount of retries */
 #define WIFI_CONNECTED_BIT BIT0
 #define WIFI_FAIL_BIT      BIT1
-
-static const char* TAG = "wifi station";
-
-static int s_retry_num = 0;
 
 void wifi_init_sta(void);
